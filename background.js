c5cd039118df52931ecf768cfd9d53072627fc59
hellodword <46193371+hellodword@users.noreply.github.com>
2022-08-09 14:32:08 +0800

    add options

--- background.js
+++ background.js
@@ -1,26 +1,16 @@
 chrome.history.onVisited.addListener(function (item) {
 
-    let url_patterns = [
-        "^https://gchq\.github\.io/CyberChef/#.*?",
-        "^https://www\.google\.com/search.*?",
-    ];
-
-    let title_patterns = [];
-
-    url_patterns.forEach((pattern) => {
-        if (new RegExp(pattern).test(item.url)) {
-            chrome.history.deleteUrl({
-                url: item.url
-            })
-        }
-    });
-
-    title_patterns.forEach((pattern) => {
-        if (new RegExp(pattern).test(item.title)) {
-            chrome.history.deleteUrl({
-                url: item.url
-            })
-        }
+    chrome.storage.sync.get({
+        patterns: ''
+    }, function (items) {
+        items.patterns.split('\n').forEach((pattern) => {
+            console.log(pattern);
+            if (pattern !== "" && new RegExp(pattern).test(item.url)) {
+                chrome.history.deleteUrl({
+                    url: item.url
+                })
+            }
+        })
     });
 
 })
