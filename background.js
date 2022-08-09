851ed2292c589841f573abf99e0f1d4e298cf890
hellodword <46193371+hellodword@users.noreply.github.com>
2022-08-09 12:39:02 +0800

    mv3

--- background.js
+++ background.js
@@ -1,9 +1,26 @@
 chrome.history.onVisited.addListener(function (item) {
 
-    if (new RegExp("^https://gchq\.github\.io/CyberChef/.*?").test(item.url)) {
-        chrome.history.deleteUrl({
-            url: item.url
-        })
-    }
+    let url_patterns = [
+        "^https://gchq\.github\.io/CyberChef/#.*?",
+        "^https://www\.google\.com/search.*?",
+    ];
+
+    let title_patterns = [];
+
+    url_patterns.forEach((pattern) => {
+        if (new RegExp(pattern).test(item.url)) {
+            chrome.history.deleteUrl({
+                url: item.url
+            })
+        }
+    });
+
+    title_patterns.forEach((pattern) => {
+        if (new RegExp(pattern).test(item.title)) {
+            chrome.history.deleteUrl({
+                url: item.url
+            })
+        }
+    });
 
 })
